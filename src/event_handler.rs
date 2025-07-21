26e9834c52c4eafcd24fb826e2346ddc12608991
asian-mario <ranggagunarto@gmail.com>
2025-07-21 16:15:54 +0800

    Fix cache invalidation in event handling.

--- src/event_handler.rs
+++ src/event_handler.rs
@@ -49,10 +49,10 @@ pub fn handle_key_event(
         KeyCode::Left => {
             app_state.cycle_sort_left();
             app_state.process_cache.invalidate();
-            app_state.process_cache.invalidate();
         }
         KeyCode::Right => {
             app_state.cycle_sort_right();
+            app_state.process_cache.invalidate();
         }
 
         KeyCode::Char('b') => {
